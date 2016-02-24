5947127fdc6d237bd4c61abb25604334627d9ea5
James Pharaoh <james@pharaoh.uk>
2016-02-24 18:42:34 +0100

    make third party library handling more intelligent

--- wbs/project/thirdparty.py
+++ wbs/project/thirdparty.py
@@ -6,6 +6,7 @@ from __future__ import with_statement
 import git
 import os
 import subprocess
+import sys
 import yaml
 
 import wbs.yamlx as yamlx
@@ -20,43 +21,84 @@ def setup ():
 	third_party_index = (
 		read_index ())
 
-	subprocess.check_call ([
-		"git",
-		"stash",
+	git_repo = (
+		git.Repo (
+			"."))
+
+	sys.stdout.write (
+		"About to set up third party libraries\n")
+
+	# create remotes
+
+	remotes_index = dict ([
+		(remote.name, remote)
+		for remote
+		in git_repo.remotes
 	])
+
+	for project_name, project_data \
+	in third_party_index.items ():
+
+		if not project_name in remotes_index:
+
+			sys.stdout.write (
+				"Create missing remote for %s\n" % (
+					project_name))
+
+			remotes_index [project_name] = (
+				git_repo.create_remote (
+					project_name,
+					project_data ["url"]))
+
+	# fetch remotes
+
+	for project_name, project_data \
+	in third_party_index.items ():
+
+		sys.stdout.write (
+			"Fetch remote: %s\n" % (
+				project_name))
+
+		git_repo.remotes [project_name].fetch (
+			project_data ["version"])
+
+		sys.stdout.write (
+			"\x1b[1A\x1b[K")
+
+	sys.stdout.write (
+		"Fetched %s remotes\n" % (
+			len (third_party_index)))
+
+	# stash changes
+
+	if git_repo.is_dirty ():
+
+		sys.stdout.write (
+			"Stashing local changes\n")
+
+		git_repo.git.stash (
+			"save")
+
+		stashed = True
+
+	else:
+
+		stashed = False
 
 	try:
 
 		for project_name, project_data \
 		in third_party_index.items ():
 
-			repository = git.Repo (".")
-
 			project_path = (
 				"third-party/%s" % (
 					project_name))
 
-			if os.path.isdir (
+			if not os.path.isdir (
 				project_path):
 
-				print (
-					"-- update %s --" % (
-						project_name))
-
-				subprocess.check_call ([
-					"git",
-					"subtree",
-					"pull",
-					"--prefix", project_path,
-					project_data ["url"],
-					project_data ["version"],
-					"--squash",
-				])
-
-			else:
-
-				print (
-					"-- add %s --" % (
+				sys.stdout.write (
+					"First time import library: %s\n" % (
 						project_name))
 
 				subprocess.check_call ([
@@ -69,12 +111,48 @@ def setup ():
 					"--squash",
 				])
 
+			else:
+
+				local_tree = (
+					git_repo
+						.head
+						.commit
+						.tree ["third-party"] [project_name])
+
+				remote_tree = (
+					git_repo
+						.remotes [project_name]
+						.refs [project_data ["version"]]
+						.commit
+						.tree)
+
+				if local_tree == remote_tree:
+					continue
+
+				sys.stdout.write (
+					"Update library: %s\n" % (
+						project_name))
+
+				subprocess.check_call ([
+					"git",
+					"subtree",
+					"merge",
+					"--prefix", project_path,
+					"%s/%s" % (project_name, project_data ["version"]),
+					"--squash",
+				])
+
 	finally:
 
-		subprocess.check_call ([
-			"git",
-			"stash",
-			"pop",
-		])
+		if stashed:
+
+			sys.stdout.write (
+				"Unstashing local changes\n")
+
+			git_repo.git.stash (
+				"pop")
+
+	print (
+		"All done")
 
 # ex: noet ts=4 filetype=python
