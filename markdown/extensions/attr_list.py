e39acbef938d4788a4a431e41ee1701226440c4c
James Pharaoh <james.pharaoh@wistla.com>
2016-10-22 17:39:28 +0000

    Squashed 'third-party/python-markdown/' changes from 5f358c0..251183e
    251183e getiterator to iter (#501)
    1550bdb Replace `getiterator` function for Python 3.6
    6e15290 Up version to 2.6.7
    5881ae5 No need to test the same thing twice.
    8384d26 Merge branch 'master' of https://github.com/waylan/Python-Markdown
    4747cf7 Don't allow equal signs in attr_list keys.
    435bb72 Fix a few spelling errors.
    
    git-subtree-dir: third-party/python-markdown
    git-subtree-split: 251183ef2817dcf09f502bea15423f6e81c5e228

--- markdown/extensions/attr_list.py
+++ markdown/extensions/attr_list.py
@@ -53,9 +53,9 @@ def _handle_word(s, t):
     return t, t
 
 _scanner = Scanner([
-    (r'[^ ]+=".*?"', _handle_double_quote),
-    (r"[^ ]+='.*?'", _handle_single_quote),
-    (r'[^ ]+=[^ =]+', _handle_key_value),
+    (r'[^ =]+=".*?"', _handle_double_quote),
+    (r"[^ =]+='.*?'", _handle_single_quote),
+    (r'[^ =]+=[^ =]+', _handle_key_value),
     (r'[^ =]+', _handle_word),
     (r' ', None)
 ])
@@ -83,7 +83,7 @@ class AttrListTreeprocessor(Treeprocessor):
                          r'\:\-\.0-9\u00b7\u0300-\u036f\u203f-\u2040]+')
 
     def run(self, doc):
-        for elem in doc.getiterator():
+        for elem in doc.iter():
             if isBlockLevel(elem.tag):
                 # Block level: check for attrs on last line of text
                 RE = self.BLOCK_RE
